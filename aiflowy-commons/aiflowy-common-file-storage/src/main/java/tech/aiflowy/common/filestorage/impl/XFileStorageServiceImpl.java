aa02f7e3806a372e3f23cd6155e7eabf7501abf4
lizhian <503457938@qq.com>
2025-04-16 11:47:16 +0800

    feat(file-storage): 集成 X-File-Storage 文件存储- 在 pom.xml 中添加 X-File-Storage 依赖 - 实现 XFileStorageServiceImpl 类，提供文件存储服务 - 支持文件上传和读取功能

---
+++ aiflowy-commons/aiflowy-common-file-storage/src/main/java/tech/aiflowy/common/filestorage/impl/XFileStorageServiceImpl.java
@@ -0,0 +1,58 @@
+package tech.aiflowy.common.filestorage.impl;
+
+import org.dromara.x.file.storage.core.FileInfo;
+import org.dromara.x.file.storage.core.get.RemoteFileInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import tech.aiflowy.common.filestorage.FileStorageService;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+
+@Component("xFileStorageServiceImpl")
+@ConditionalOnBean(org.dromara.x.file.storage.core.FileStorageService.class)
+public class XFileStorageServiceImpl implements FileStorageService {
+
+    /**
+     * 指定存储平台
+     */
+    @Value("${aiflowy.storage.x-file-storage.platform}")
+    private String platform;
+
+    @Autowired
+    private org.dromara.x.file.storage.core.FileStorageService xFileStorageService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+    }
+
+
+    @Override
+    public String save(MultipartFile file) {
+        try {
+            FileInfo upload = xFileStorageService.of(file)
+                    .setPlatform(platform)
+                    .upload();
+            return upload.getBasePath() + upload.getPath() + upload.getFilename();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public InputStream readStream(String path) {
+        RemoteFileInfo remoteFileInfo = xFileStorageService.getFile()
+                .setPlatform(platform)
+                .setFilename(path)
+                .getFile();
+        byte[] bytes = xFileStorageService.download(remoteFileInfo.toFileInfo()).bytes();
+        return new ByteArrayInputStream(bytes);
+    }
+}
