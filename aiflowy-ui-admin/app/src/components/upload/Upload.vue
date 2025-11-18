0d8cb5de207d7423ef1cde0aa8ef582d565bc519
18586361686 <1207669650@qq.com>
2025-11-18 17:17:55 +0800

    chore: 文件上传组件

--- aiflowy-ui-admin/app/src/components/upload/Upload.vue
+++ aiflowy-ui-admin/app/src/components/upload/Upload.vue
@@ -1,5 +1,76 @@
+<script lang="ts" setup>
+import type { UploadProps, UploadUserFile } from 'element-plus';
+
+import { defineEmits, ref } from 'vue';
+
+import { useAppConfig } from '@aiflowy/hooks';
+import { useAccessStore } from '@aiflowy/stores';
+
+import { ElButton, ElMessage, ElMessageBox, ElUpload } from 'element-plus';
+
+const props = defineProps({
+  action: {
+    type: String,
+    default: '/api/v1/commons/upload',
+  },
+  tips: {
+    type: String,
+    default: '',
+  },
+  limit: {
+    type: Number,
+    default: 3,
+  },
+  multiple: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const emit = defineEmits([
+  'success', // 文件上传成功
+  'handleDelete',
+  'handlePreview',
+]);
+
+const { apiURL } = useAppConfig(import.meta.env, import.meta.env.PROD);
+
+const accessStore = useAccessStore();
+const headers = ref({
+  'aiflowy-token': accessStore.accessToken,
+});
+const fileList = ref<UploadUserFile[]>([]);
+
+const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
+  emit('handleDelete', file, uploadFiles);
+};
+
+const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
+  emit('handleDelete', uploadFile);
+};
+
+const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
+  ElMessage.warning(
+    `The limit is 3, you selected ${files.length} files this time, add up to ${
+      files.length + uploadFiles.length
+    } totally`,
+  );
+};
+
+const handleSuccess: UploadProps['onSuccess'] = (response) => {
+  emit('success', response);
+};
+
+const beforeRemove: UploadProps['beforeRemove'] = (uploadFile) => {
+  return ElMessageBox.confirm(`确定删除 ${uploadFile.name} ?`).then(
+    () => true,
+    () => false,
+  );
+};
+</script>
+
 <template>
-  <el-upload
+  <ElUpload
     v-model:file-list="fileList"
     class="upload-demo"
     :headers="headers"
@@ -12,79 +83,11 @@
     :on-exceed="handleExceed"
     :on-success="handleSuccess"
   >
-    <el-button type="primary">Click to upload</el-button>
+    <ElButton type="primary">Click to upload</ElButton>
     <template #tip>
       <div class="el-upload__tip">
-        {{tips}}
+        {{ tips }}
       </div>
     </template>
-  </el-upload>
+  </ElUpload>
 </template>
-
-<script lang="ts" setup>
-import {defineEmits, ref} from 'vue'
-import {ElMessage, ElMessageBox, ElButton, ElUpload} from 'element-plus'
-
-const {apiURL} = useAppConfig(import.meta.env, import.meta.env.PROD);
-
-import type {UploadProps, UploadUserFile} from 'element-plus'
-import {useAccessStore} from "@aiflowy/stores";
-import {useAppConfig} from "@aiflowy/hooks";
-
-const accessStore = useAccessStore();
-const props = defineProps({
-  action: {
-    type: String,
-    default: '/api/v1/commons/upload'
-  },
-  tips: {
-    type: String,
-    default: ''
-  },
-  limit: {
-    type: Number,
-    default: 3
-  },
-  multiple: {
-    type: Boolean,
-    default: true
-  }
-})
-const headers = ref({
-  'aiflowy-token': accessStore.accessToken
-})
-const fileList = ref<UploadUserFile[]>([])
-
-const emit = defineEmits([
-  'success',       // 文件上传成功
-])
-
-const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
-  console.log(file, uploadFiles)
-}
-
-const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
-  console.log(uploadFile)
-}
-
-const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
-  ElMessage.warning(
-    `The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + uploadFiles.length
-    } totally`
-  )
-}
-
-const handleSuccess: UploadProps['onSuccess'] = (response) => {
-  emit('success', response)
-}
-
-const beforeRemove: UploadProps['beforeRemove'] = (uploadFile) => {
-  return ElMessageBox.confirm(
-    `确定删除 ${uploadFile.name} ?`
-  ).then(
-    () => true,
-    () => false
-  )
-}
-</script>
