582eb1825b54aa40db9265be7301e10ed1a8fc46
john <409225776@qq.com>
2025-11-25 10:58:57 +0800

    feat：数据中枢表数据增删改查

--- aiflowy-ui-admin/app/src/views/datacenter/DatacenterTableDetail.vue
+++ aiflowy-ui-admin/app/src/views/datacenter/DatacenterTableDetail.vue
@@ -4,13 +4,22 @@ import { useRoute } from 'vue-router';
 
 import { $t } from '@aiflowy/locales';
 
-import { ArrowLeft, Delete, Edit, Refresh } from '@element-plus/icons-vue';
+import {
+  ArrowLeft,
+  Delete,
+  Edit,
+  Plus,
+  Refresh,
+  Upload,
+} from '@element-plus/icons-vue';
 import {
   ElButton,
   ElCol,
   ElIcon,
   ElMenu,
   ElMenuItem,
+  ElMessage,
+  ElMessageBox,
   ElRow,
   ElTable,
   ElTableColumn,
@@ -20,6 +29,7 @@ import { api } from '#/api/request';
 import tableIcon from '#/assets/datacenter/table2x.png';
 import PageData from '#/components/page/PageData.vue';
 import { router } from '#/router';
+import RecordModal from '#/views/datacenter/RecordModal.vue';
 
 const pageDataRef = ref();
 const route = useRoute();
@@ -32,6 +42,7 @@ const detailInfo = ref<any>({});
 const fieldList = ref<any[]>([]);
 const headers = ref<any[]>([]);
 const activeMenu = ref('1');
+const recordModal = ref();
 function getDetailInfo(id: any) {
   api.get(`/api/v1/datacenterTable/detailInfo?tableId=${id}`).then((res) => {
     detailInfo.value = res.data;
@@ -44,10 +55,43 @@ function getHeaders(id: any) {
   });
 }
 function showDialog(row: any) {
-  console.log(row);
+  recordModal.value.openDialog({ ...row });
 }
 function remove(row: any) {
-  console.log(row);
+  ElMessageBox.confirm($t('message.deleteAlert'), $t('message.noticeTitle'), {
+    confirmButtonText: $t('message.ok'),
+    cancelButtonText: $t('message.cancel'),
+    type: 'warning',
+    beforeClose: (action, instance, done) => {
+      if (action === 'confirm') {
+        instance.confirmButtonLoading = true;
+        api
+          .post(
+            `/api/v1/datacenterTable/removeValue`,
+            {},
+            {
+              params: {
+                tableId: tableId.value,
+                id: row.id,
+              },
+            },
+          )
+          .then((res) => {
+            instance.confirmButtonLoading = false;
+            if (res.errorCode === 0) {
+              ElMessage.success(res.message);
+              refresh();
+              done();
+            }
+          })
+          .catch(() => {
+            instance.confirmButtonLoading = false;
+          });
+      } else {
+        done();
+      }
+    },
+  }).catch(() => {});
 }
 function refresh() {
   pageDataRef.value.setQuery({});
@@ -56,16 +100,38 @@ function refresh() {
 
 <template>
   <div class="page-container">
+    <RecordModal
+      ref="recordModal"
+      :form-items="headers"
+      :table-id="tableId"
+      @reload="refresh"
+    />
     <ElRow>
       <ElCol :span="24" class="border-b">
-        <div class="mb-2.5 flex items-center gap-2">
-          <ElIcon class="cursor-pointer" @click="router.back()">
-            <ArrowLeft />
-          </ElIcon>
-          <img :src="tableIcon" class="h-10 w-10" />
-          <div class="flex flex-col justify-center">
-            <div class="text-sm font-bold">{{ detailInfo.tableName }}</div>
-            <div class="desc text-xs">{{ detailInfo.tableDesc }}</div>
+        <div class="mb-2.5 flex items-center justify-between">
+          <div class="flex items-center gap-2.5">
+            <ElIcon class="cursor-pointer" @click="router.back()">
+              <ArrowLeft />
+            </ElIcon>
+            <img :src="tableIcon" class="h-10 w-10" />
+            <div class="flex flex-col justify-center">
+              <div class="text-sm font-bold">{{ detailInfo.tableName }}</div>
+              <div class="desc text-xs">{{ detailInfo.tableDesc }}</div>
+            </div>
+          </div>
+          <div class="flex items-center" v-if="activeMenu === '2'">
+            <ElButton type="primary" @click="showDialog({})">
+              <ElIcon class="mr-1">
+                <Plus />
+              </ElIcon>
+              {{ $t('button.addLine') }}
+            </ElButton>
+            <ElButton type="primary" @click="refresh">
+              <ElIcon class="mr-1">
+                <Upload />
+              </ElIcon>
+              {{ $t('button.batchImport') }}
+            </ElButton>
           </div>
         </div>
       </ElCol>
@@ -114,7 +180,7 @@ function refresh() {
           :page-size="10"
         >
           <template #default="{ pageList }">
-            <ElButton @click="refresh" class="float-right">
+            <ElButton link @click="refresh" class="float-right">
               <ElIcon>
                 <Refresh />
               </ElIcon>
@@ -127,7 +193,13 @@ function refresh() {
                 :label="item.title"
               >
                 <template #default="{ row }">
-                  {{ row[item.key] }}
+                  <div v-if="item.fieldType === 5">
+                    {{ 1 === row[item.key] ? '是' : '否' }}
+                  </div>
+                  <div v-else-if="item.fieldType === 4">
+                    {{ parseFloat(row[item.key]) }}
+                  </div>
+                  <div v-else>{{ row[item.key] }}</div>
                 </template>
               </ElTableColumn>
               <ElTableColumn :label="$t('common.handle')" width="150">
---
+++ aiflowy-ui-admin/app/src/views/datacenter/RecordModal.vue
@@ -0,0 +1,159 @@
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus';
+
+import { onMounted, ref } from 'vue';
+
+import {
+  ElButton,
+  ElDatePicker,
+  ElDialog,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElInputNumber,
+  ElMessage,
+} from 'element-plus';
+
+import { api } from '#/api/request';
+import DictSelect from '#/components/dict/DictSelect.vue';
+import { $t } from '#/locales';
+
+const props = withDefaults(defineProps<RecordModalProps>(), {});
+const emit = defineEmits(['reload']);
+// vue
+export interface RecordModalProps {
+  formItems: any[];
+  tableId: any;
+}
+onMounted(() => {});
+defineExpose({
+  openDialog,
+});
+const saveForm = ref<FormInstance>();
+// variables
+const dialogVisible = ref(false);
+const isAdd = ref(true);
+const entity = ref<any>({});
+const btnLoading = ref(false);
+const rules = ref({});
+
+// functions
+function openDialog(row: any) {
+  if (row.id) {
+    isAdd.value = false;
+  }
+  entity.value = row;
+  dialogVisible.value = true;
+}
+function save() {
+  saveForm.value?.validate((valid) => {
+    if (valid) {
+      const data: Record<string, any> = {};
+      for (const formItem of props.formItems) {
+        data[formItem.key] = entity.value[formItem.key];
+      }
+      data.tableId = props.tableId;
+      data.id = entity.value.id;
+      api
+        .post(
+          '/api/v1/datacenterTable/saveValue',
+          {},
+          {
+            params: data,
+          },
+        )
+        .then((res) => {
+          btnLoading.value = false;
+          if (res.errorCode === 0) {
+            ElMessage.success(res.message);
+            emit('reload');
+            closeDialog();
+          }
+        })
+        .catch(() => {
+          btnLoading.value = false;
+        });
+    }
+  });
+}
+function closeDialog() {
+  saveForm.value?.resetFields();
+  isAdd.value = true;
+  entity.value = {};
+  dialogVisible.value = false;
+}
+</script>
+
+<template>
+  <ElDialog
+    v-model="dialogVisible"
+    draggable
+    :title="isAdd ? $t('button.add') : $t('button.edit')"
+    :before-close="closeDialog"
+    :close-on-click-modal="false"
+    width="800px"
+  >
+    <ElForm
+      label-width="100px"
+      ref="saveForm"
+      :model="entity"
+      status-icon
+      :rules="rules"
+    >
+      <ElFormItem
+        v-for="item in props.formItems"
+        :rules="
+          item.required
+            ? [
+                {
+                  required: true,
+                  message: `${item.title}${$t('message.notEmpty')}`,
+                },
+              ]
+            : []
+        "
+        :key="item.key"
+        :prop="item.key"
+        :label="item.title"
+      >
+        <ElInput v-if="item.fieldType === 1" v-model.trim="entity[item.key]" />
+        <ElInputNumber
+          v-if="item.fieldType === 2"
+          v-model.number="entity[item.key]"
+          :precision="0"
+        />
+        <ElDatePicker
+          v-if="item.fieldType === 3"
+          v-model="entity[item.key]"
+          type="datetime"
+          value-format="YYYY-MM-DD HH:mm:ss"
+        />
+        <ElInputNumber
+          v-if="item.fieldType === 4"
+          v-model="entity[item.key]"
+          :precision="2"
+        />
+        <DictSelect
+          v-if="item.fieldType === 5"
+          v-model="entity[item.key]"
+          dict-code="yesOrNo"
+        />
+      </ElFormItem>
+    </ElForm>
+    <template #footer>
+      <ElButton @click="closeDialog">
+        {{ $t('button.cancel') }}
+      </ElButton>
+      <ElButton
+        type="primary"
+        @click="save"
+        :loading="btnLoading"
+        :disabled="btnLoading"
+      >
+        {{ $t('button.save') }}
+      </ElButton>
+    </template>
+  </ElDialog>
+</template>
+
+<style scoped></style>
