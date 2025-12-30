21ca2f7e01a7999cf1c82b4baf2cb439f8e46038
18586361686 <1207669650@qq.com>
2025-12-30 11:51:03 +0800

    feat: userCenter UcUserFeedbackController

---
+++ aiflowy-api/aiflowy-api-usercenter/src/main/java/tech/aiflowy/usercenter/controller/system/UcUserFeedbackController.java
@@ -0,0 +1,23 @@
+package tech.aiflowy.usercenter.controller.system;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.aiflowy.common.web.controller.BaseCurdController;
+import tech.aiflowy.system.entity.SysUserFeedback;
+import tech.aiflowy.system.service.SysUserFeedbackService;
+
+
+/**
+ *  控制层。
+ *
+ * @author 12076
+ * @since 2025-12-30
+ */
+@RestController
+@RequestMapping("/userCenter/sysUserFeedback")
+public class UcUserFeedbackController extends BaseCurdController<SysUserFeedbackService, SysUserFeedback> {
+    public UcUserFeedbackController(SysUserFeedbackService service) {
+        super(service);
+    }
+
+}
