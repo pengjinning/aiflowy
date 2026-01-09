ccb9b93f8b87c4f5e1d1092e560d384d8ef62261
18586361686 <1207669650@qq.com>
2026-01-09 11:05:19 +0800

    chore: chore code

--- aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/config/McpClientAutoConfig.java
+++ aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/config/McpClientAutoConfig.java
@@ -1,18 +1,14 @@
 package tech.aiflowy.ai.config;
 
 import com.agentsflex.mcp.client.McpClientManager;
-import io.modelcontextprotocol.client.McpSyncClient;
-import io.modelcontextprotocol.spec.McpSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import tech.aiflowy.ai.entity.Mcp;
 import tech.aiflowy.ai.service.impl.McpServiceImpl;
-import tech.aiflowy.common.web.exceptions.BusinessException;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Optional;
@@ -32,27 +28,27 @@ public class McpClientAutoConfig {
     @PostConstruct
     public void initMcpClient() {
         log.info("开始初始化 MCP 客户端...");
-        try {
-            List<Mcp> mcpList = mcpService.list();
-            log.info("获取到 MCP 配置列表，数量：{}", mcpList.size());
-
-            mcpList.forEach(mcp ->  {
-                if (!mcp.getStatus()) {
-                    return;
-                }
-                String configJson = mcp.getConfigJson();
-                Optional<String> firstServerName = getFirstMcpServerName(configJson);
-                if (firstServerName.isPresent()) {
-                    String serverName = firstServerName.get();
+        List<Mcp> mcpList = mcpService.list();
+        log.info("获取到 MCP 配置列表，数量：{}", mcpList.size());
+        mcpList.forEach(mcp -> {
+            if (!mcp.getStatus()) {
+                return;
+            }
+            String configJson = mcp.getConfigJson();
+            Optional<String> firstServerName = getFirstMcpServerName(configJson);
+            if (firstServerName.isPresent()) {
+                String serverName = firstServerName.get();
+                try {
                     mcpClientManager.registerFromJson(configJson);
-                    log.info("MCP服务名称：{} 注册成功", serverName);
-                } else {
-                    log.error("MCP服务名称为：{} 启动失败，配置 JSON 中未找到服务名称", mcp.getTitle());
+                } catch (Exception e) {
+                    log.error("MCP服务名称：{} 注册失败", serverName, e);
                 }
-            });
-        } catch (Exception e) {
-            log.error("初始化 MCP 客户端失败", e);
-        }
+                log.info("MCP服务名称：{} 注册成功", serverName);
+            } else {
+                log.error("MCP服务名称为：{} 启动失败，配置 JSON 中未找到服务名称", mcp.getTitle());
+            }
+        });
+
     }
 
 }
